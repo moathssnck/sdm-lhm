dfcbf23d5d597ea6d58f33fda45b168e761fc1e6
moathssnck <myneww00@gmail.com>
2025-07-27 17:59:37 +0300

    hg

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import { CartProvider } from "@/hooks/use-cart"
 import { Toaster } from "@/components/ui/toaster"
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "ذبيحتي - أجود أنواع اللحوم الطازجة",
@@ -21,6 +22,12 @@ export default function RootLayout({
           {children}
           <Toaster />
         </CartProvider>
+        <Script
+        id="tawk-to-chat-widget"
+        src="https://embed.tawk.to/68863bf1f072f7192b20d434/1j165ka79"
+        strategy="lazyOnload" // Load the script after the page becomes interactive
+     
+      />
       </body>
     </html>
   )
--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,16 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { doc, getFirestore, setDoc } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDd2EMKpBE97LZi0c1bZuStolVcKjC3nMI",
-  authDomain: "blbl-603c1.firebaseapp.com",
-  databaseURL: "https://blbl-603c1-default-rtdb.firebaseio.com",
-  projectId: "blbl-603c1",
-  storageBucket: "blbl-603c1.firebasestorage.app",
-  messagingSenderId: "909940493222",
-  appId: "1:909940493222:web:e294c9e3690b8d9abfdd67",
-  measurementId: "G-7WJTZ4BZPE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
